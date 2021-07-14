d287a9297c07fdfdce1a6daebe5ffffe2cff3988
Diana Rybikov <diana.ryb214@gmail.com>
2021-07-14 13:30:38 -0600

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+ph = Product.create(
+    name: 'Phone Holder',
+    description: 'Attaches to the dashboard with no damage. Place anyway and see your phone, directions, etc.',
+    image: "https://i.pinimg.com/originals/19/f1/1d/19f11dd0629990ca1d8119f2c0cfbd1b.jpg"
+)
+af = Product.create(
+    name: 'Air Freshener',
+    description: 'Everyone likes a good smelling car.',
+    image: "https://i.pinimg.com/736x/55/76/33/5576336d57584b172370dde72e6784de.jpg"
+)
+saf = Product.create(
+    name: 'Hanging Air Freshener',
+    description: 'Everyone likes a good smelling care.',
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9-sJUwf9osnZ3MHR5hoafkJCrMjIfhB6FCA&usqp=CAU"
+)
+wt = Product.create(
+    name: 'Wheel Cover',
+    description: 'Keep your wheel warm!',
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQadsBCnuJDDHo27b9jdIV-Xad7IvRPQ0kIhw&usqp=CAU"
+)
+fm = Product.create(
+    name: 'Floor Mats',
+    description: 'Add a good looking Floor MAt',
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSDEEKFAImaH5ldw3p2Ao4ET0modc040PUIg&usqp=CAU"
+)
+led = Product.create(
+    name: 'Led lights',
+    description: 'Light it up.',
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMDbSDSPkIEOxtyLRF2VrnSLgFGbx6kyOunQ&usqp=CAU"
+)
